837c06c1b5f3a74eb532502fd553784991bb4fd1
Guillaume Bono <guillaume.bono@insa-lyon.fr>
2021-05-25 14:36:52 +0200

    Minor fix to locobot_controller_manager

--- locobot_hardware_interface/src/locobot_controller_manager.cpp
+++ locobot_hardware_interface/src/locobot_controller_manager.cpp
@@ -8,18 +8,26 @@ int main(int argc, char** argv)
     ros::init(argc, argv, "locobot_controller_manager");
     ros::NodeHandle root_nh;
     ros::NodeHandle priv_nh("~");
+
     LocobotHW locobot;
     if (!locobot.init(root_nh, priv_nh)){ return 1; }
-    controller_manager::ControllerManager cm(&locobot);
+
+    controller_manager::ControllerManager cm(&locobot, root_nh);
+
     ros::Rate rate(30);
     ros::Time last_time = ros::Time::now();
     ros::Time cur_time;
+    ros::Duration elapsed;
+
     while(ros::ok())
     {
         cur_time = ros::Time::now();
-        locobot.read(cur_time, cur_time - last_time);
-        cm.update(cur_time, cur_time - last_time);
-        locobot.write(cur_time, cur_time - last_time);
+        elapsed = cur_time - last_time;
+
+        locobot.read(cur_time, elapsed);
+        cm.update(cur_time, elapsed);
+        locobot.write(cur_time, elapsed);
+
         last_time = cur_time;
         rate.sleep();
     }
